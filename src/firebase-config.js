e8f2852e6e580f0f1ea05faf09dc38f5cf745639
ctang415 <carrietang117@gmail.com>
2023-04-04 23:09:09 -0400

    Initial set up for react project with firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAf7xaPQwKVDxEvztszhdAHtOIdyVTiUNU",
+  authDomain: "reddit-clone-e4070.firebaseapp.com",
+  projectId: "reddit-clone-e4070",
+  storageBucket: "reddit-clone-e4070.appspot.com",
+  messagingSenderId: "708320944955",
+  appId: "1:708320944955:web:c1a8086cc645f34ef7cd1e"
+};
+
+const app = initializeApp(firebaseConfig);
